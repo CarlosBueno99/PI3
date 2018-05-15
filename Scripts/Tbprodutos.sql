2d197354007e5432358c9d2d4d07a166b46f2e54
diamantino960 <the_te_us@hotmail.com>
2018-05-15 10:16:51 -0300

    Atualização tbprodutos
    15/05 10:16

--- Scripts/Tbprodutos.sql
+++ Scripts/Tbprodutos.sql
@@ -5,10 +5,11 @@ id int primary key not null auto_increment,
 nome varchar(30),
 tipo varchar(20),
 marca varchar(20),
-sku varchar(20),
-precovenda double,
-precocompra double);
-
+sku varchar(20) unique,
+precovenda varchar(30),
+precocompra varchar(30),
+quantidade int
+);
 
 
 select * from tbprodutos;
