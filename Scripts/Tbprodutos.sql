5a999c6726d5cae46621ada1bf1c6556d8571708
diamantino960 <the_te_us@hotmail.com>
2018-06-12 19:33:42 -0300

    SCRIPT
    Produto

--- Scripts/Tbprodutos.sql
+++ Scripts/Tbprodutos.sql
@@ -14,36 +14,32 @@ marca varchar(20),
 
 sku varchar(20) unique,
 
-precovenda varchar(30),
-
-precocompra varchar(30),
-
-quantidade int
+precovenda varchar(30)
 );
 
-insert into tbprodutos (nome, tipo, marca, sku, precovenda, precocompra, quantidade)
-value ('zomo watermelon  mint', 'essencia', 'zomo', '1', '10,00', '6,50', '40');
+insert into tbprodutos(nome, tipo, marca, sku, precovenda)
+value ('X', 'mangueira', 'triton','triton_X', '75.00');
 
-insert into tbprodutos (nome, tipo, marca, sku, precovenda, precocompra, quantidade)
-value ('zomo guava', 'essencia', 'zomo', '2', '10,00', '6,50', '20');
+insert into tbprodutos(nome, tipo, marca, sku, precovenda)
+value ('invoke', 'rosh', 'mahalla','mahalla_invoke', '35.00');
 
-insert into tbprodutos (nome, tipo, marca, sku, precovenda, precocompra, quantidade)
-value ('adalya swiss bombom', 'essencia', 'adalya', '3', '16,00', '9,50', '10');
+insert into tbprodutos(nome, tipo, marca, sku, precovenda)
+value ('aluminio', 'aluminio', 'predator','predator_aluminio', '15.00');
 
-insert into tbprodutos (nome, tipo, marca, sku, precovenda, precocompra, quantidade)
-value ('vgod mint', 'essencia', 'vgod', '4', '12,00', '8,00', '30');
+insert into tbprodutos(nome, tipo, marca, sku, precovenda)
+value ('orange', 'essencia', 'mazaya','orange_mazaya', '9.00');
 
-insert into tbprodutos (nome, tipo, marca, sku, precovenda, precocompra, quantidade)
-value ('tangiers new lemon lime', 'essencia', 'tangiers', '5', '150,00', '95,00', '1');
+insert into tbprodutos(nome, tipo, marca, sku, precovenda)
+value ('pinepas', 'essencia', 'tangiers','tangiers_pinepas', '150.00');
 
-insert into tbprodutos (nome, tipo, marca, sku, precovenda, precocompra, quantidade)
-value ('regal queen', 'stem', 'regal hookah', '6', '1100,00', '850,00', '2');
+insert into tbprodutos(nome, tipo, marca, sku, precovenda)
+value ('guava', 'essencia', 'zomo','zomo_guava', '11.00');
 
-insert into tbprodutos (nome, tipo, marca, sku, precovenda, precocompra, quantidade)
-value ('boho yunan rubi', 'vaso', 'egermann', '7', '510,00', '350,00', '3');
+insert into tbprodutos(nome, tipo, marca, sku, precovenda)
+value ('grape', 'essencia', 'zomo','zomo_grape', '11.00');
 
-insert into tbprodutos (nome, tipo, marca, sku, precovenda, precocompra, quantidade)
-value ('imperial hk', 'prato', 'hookah king', '8', '150,00', '100,00', '5');
+insert into tbprodutos(nome, tipo, marca, sku, precovenda)
+value ('mint', 'essencia', 'zomo','zomo_mint', '11.00');
 
 
 select * from tbprodutos;
